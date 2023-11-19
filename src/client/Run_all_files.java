919253a7ea1d4dd7d8fa9be04f439d3e43a9ebf5
OziomaEunice <onwubikooziomaeunice@gmail.com>
2023-11-19 20:07:19 +0000

    changed code to run the constructor of the class

--- src/client/Run_all_files.java
+++ src/client/Run_all_files.java
@@ -3,7 +3,8 @@ package client;
 // Create the main method --- this will execute the MainFrame class
 public class Run_all_files{
     public static void main(String[] args) {
-        MainFrame mainFrame = new MainFrame();
+        
+        new MainFrame();   // runs the MainFrame constructor 
         
     }
 }
